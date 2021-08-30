80b17ed5614ed62b3b3eb09ceffc0c0c793f0cca
Mikhail Ostrovskyi <ostrovskyi@meridianpalace.com>
2021-08-30 19:33:58 +0300

    Added test for has_state call without block

--- spec/lib/has_state/inclusions/class_methods_spec.rb
+++ spec/lib/has_state/inclusions/class_methods_spec.rb
@@ -6,16 +6,29 @@ RSpec.describe HasState::Inclusions::ClassMethods do
   describe '.has_state' do
     subject do
       lambda do
-        # rubocop:disable Lint/ConstantDefinitionInBlock
         class TestDeclaration
           extend HasState::Inclusions::ClassMethods
 
-          has_state
+          has_state do
+            puts 'block passed'
+          end
         end
-        # rubocop:enable Lint/ConstantDefinitionInBlock
       end
     end
 
     it { is_expected.not_to raise_error }
+
+    context 'when block is not passed' do
+      subject do
+        lambda do
+          class TestDeclaration
+            extend HasState::Inclusions::ClassMethods
+            has_state
+          end
+        end
+      end
+
+      it { is_expected.not_to raise_error }
+    end
   end
 end
