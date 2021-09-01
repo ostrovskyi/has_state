10eecf6c82630bf36601b0634037d50db1adefb0
Mikhail Ostrovskyi <ostrovskyi@meridianpalace.com>
2021-09-01 20:28:25 +0300

    Add module integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,19 +2,35 @@
 
 require 'has_state'
 
-class Vehicle
-  include HasState
+RSpec.describe 'module integration' do
+  describe 'without any params' do
+    class Championship
+      include HasState
 
-  has_state
-end
+      has_state
+    end
 
-RSpec.describe Vehicle do
-  subject { described_class.new }
+    subject { Championship.new }
 
-  describe '#state' do
-    it do
-      is_expected.to respond_to(:state)
-      expect(subject.state).to eq('initial')
+    describe '#state' do
+      it do
+        is_expected.to respond_to(:state)
+        expect(subject.state).to eq(:initial)
+      end
+    end
+  end
+
+  describe 'specifying custom initial value' do
+    class ChampionshipCustomValue
+      include HasState
+
+      has_state default: :group_phase
+    end
+
+    subject { ChampionshipCustomValue.new.state }
+
+    describe '#state' do
+      it { is_expected.to eq(:group_phase) }
     end
   end
 end
