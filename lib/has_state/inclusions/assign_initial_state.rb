3ba3e15af0a77a46302ae6586465becf401f79a1
Mikhail Ostrovskyi <ostrovskyi@meridianpalace.com>
2021-09-11 12:12:22 +0300

    Fix rubocop issues

--- lib/has_state/inclusions/assign_initial_state.rb
+++ lib/has_state/inclusions/assign_initial_state.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module HasState
   module Inclusions
     module AssignInitialState
@@ -7,7 +9,7 @@ module HasState
         options = self.class.class_variable_get(:@@options_provider).options
         field_name = options[:field_name]
         default_value = options[:default_value]
-        self.class.class_eval{ attr_reader field_name }
+        self.class.class_eval { attr_reader field_name }
         instance_variable_set("@#{field_name}".to_sym, default_value) unless send(field_name)
       end
     end
