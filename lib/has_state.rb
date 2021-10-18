b7c4af5ca6578bd833c246077f601829b7bef8f2
Mikhail Ostrovskyi <ostrovskyi@meridianpalace.com>
2021-10-18 21:28:17 +0300

    Remove Inclusions::Base

--- lib/has_state.rb
+++ lib/has_state.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'has_state/assign_initial_value'
 require_relative 'has_state/configurator'
 require_relative 'has_state/declarations'
-require_relative 'has_state/inclusions/base'
 require_relative 'has_state/inclusions/class_methods'
 
 module HasState
-  include Inclusions::Base
-
   def self.included(base)
     base.extend Declarations
     base.extend Inclusions::ClassMethods
+
+    base.include AssignInitialValue
   end
 end
---
+++ lib/has_state/assign_initial_value.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module HasState
+  module AssignInitialValue
+    private
+
+    def has_state_assign_initial_value
+      options = self.class.class_variable_get(:@@options_provider).options
+      field_name = options[:field_name]
+      instance_variable_set("@#{field_name}", has_state_initial_value)
+    end
+
+    def has_state_initial_value
+      options = self.class.class_variable_get(:@@options_provider).options
+      field_name = options[:field_name]
+      default_value = options[:default_value]
+      current_value = send(field_name)
+
+      current_value || default_value
+    end
+  end
+end
